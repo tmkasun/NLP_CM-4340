15a5118a7146dc8f84bdaa64c26009162685eea6
tmkasun <tmkasun@gmail.com>
2015-06-29 20:30:15 +0530

    Update LMS data extractor

--- regex_subject_stats/lms_extractor.py
+++ regex_subject_stats/lms_extractor.py
@@ -1,1 +1,15 @@
 __author__ = 'tmkasun'
+
+import requests
+import re
+
+cookies = dict(PHPSESSID='uullf27nsenm4p35i1jnhh9pr6')
+url = u'http://lms.mrt.ac.lk/student_enrolment_window.php?stu_UserID=10204'
+request_result = requests.get(url, cookies=cookies)
+cont = request_result.content
+
+pattern1 = re.compile('UNIVERISTY OF MORATUWA - STUDENT ENROLLMENTS')
+
+a = re.search(pattern1,cont)
+
+print(a)
